f4b9044aeb4b8a17a585a094b471f6b0c77ae15f
Gioanna Rheumer <ratofkryll@gmail.com>
2021-10-09 21:06:18 -0700

    start on filter_duplicate_rows, put on hold to re-think it

--- lib/csv_processor.rb
+++ lib/csv_processor.rb
@@ -4,6 +4,8 @@ class CsvProcessor
   # Open a CSV and return a collection we can work with
   def parse_file(csv)
     data = []
+    errors = []
+    invalid_rows = 0
 
     CSV.foreach(csv, headers: true, header_converters: :symbol).with_index(1) do |row, i|
       row_hash = row.to_h
@@ -12,10 +14,12 @@ class CsvProcessor
       # This way will eat a lot of memory for large files. TODO: Refactor for large file handling
         data << row_hash
       else
-        puts "Row #{i} is missing information."
+        invalid_rows += 1
+        errors << "Row #{i} is missing contact info.\n"
       end
     end
-
+    # filter_duplicate_rows(data)
+    print_report(33, 2, invalid_rows, errors)
     data
   end
 
@@ -28,5 +32,22 @@ class CsvProcessor
 
     # Returns false if any of the required keys are nil or an empty string. In Rails, you could use .blank? to check for both nil and an empty string
     required_keys.all? { |x| !row_hash[x].nil? && !row_hash[x].empty? }
+  end
+
+  # def filter_duplicate_rows(csv_data)
+  #   grouped_data = csv_data.group_by { |row| [row[:first_name], row[:last_name], row[:email]] }
+  #
+  #   grouped_data.transform_values(&:count)
+  #
+  #   puts grouped_data
+  # end
+
+  # Prints a report of the contacts to the console
+  def print_report(total_contacts, duplicate_contacts, invalid_contacts, errors)
+    print "Total Contacts: #{total_contacts}\nDuplicate Contacts: #{duplicate_contacts}\nInvalid Contacts: #{invalid_contacts}\n"
+
+    errors.each do |error|
+      print error
+    end
   end
 end
--- spec/lib/csv_processor_spec.rb
+++ spec/lib/csv_processor_spec.rb
@@ -2,11 +2,25 @@ require 'csv_processor'
 
 RSpec.describe 'csv_processor' do
   csv_processor = CsvProcessor.new
-  row_hash = csv_processor.parse_file('exercise/contacts.csv').first
+  row_hash = {
+    :first_name=>"Glenda",
+    :last_name=>"Rosas",
+    :email=>"glendarrosas@dodgit.com",
+    :phone=>"613-937-0694",
+    :address_line_1=>"326 Donato Ridges",
+    :city=>"Rimbey",
+    :province=>"AB",
+    :country_name=>"Canada",
+    :postcode=>"T0C 2J0",
+    :date_added=>"2015-02-24 10:54:02 UTC",
+    :how_did_you_hear_about_us=>"Newspaper",
+    :what_is_your_budget=>"$200-$299",
+    :what_is_your_favourite_color=>"Blue"
+    }
 
   # This assumes we are always using the same test CSV
   it 'reads and outputs each row of the csv' do
-    expect(row_hash).to eq({
+    expect(csv_processor.parse_file('exercise/contacts.csv').first).to eq({
       :first_name=>"Glenda",
       :last_name=>"Rosas",
       :email=>"glendarrosas@dodgit.com",
